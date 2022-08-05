10ef3701a520ee89549032a3f92f872a7ec99dd6
Ziy Shem <dergesus@gmail.com>
2022-08-05 21:36:25 +0400

    echo command

--- app/server.go
+++ app/server.go
@@ -4,7 +4,21 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Command string
+
+const (
+	CommandPing Command = "PING"
+	CommandEcho Command = "ECHO"
+)
+
+type Request struct {
+	cmd  Command
+	args []string
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -27,14 +41,34 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	for {
-		_, err := conn.Read(nil)
+		var buf []byte
+		n, err := conn.Read(buf)
 		if err != nil {
 			fmt.Println("Error reading from connection: ", err.Error())
 			os.Exit(1)
 		}
 
+		str := string(buf[:n])
+		fmt.Println("Received: ", str)
+
+		req, err := parseRequest(str)
+		if err != nil {
+			conn.Write([]byte("-ERR invalid request\r\n"))
+			continue
+		}
+
+		var resp []byte
+		switch req.cmd {
+		case CommandPing:
+			resp = []byte("+PONG\r\n")
+		case CommandEcho:
+			resp = []byte("+" + strings.Join(req.args, " ") + "\r\n")
+		default:
+			resp = []byte("-ERR invalid request\r\n")
+		}
+
 		// send redis PONG to client
-		n, err := conn.Write([]byte("+PONG\r\n"))
+		n, err = conn.Write(resp)
 		if err != nil {
 			fmt.Println("Error writing to socket: ", err.Error())
 			os.Exit(1)
@@ -42,4 +76,54 @@ func handleConnection(conn net.Conn) {
 
 		fmt.Printf("Wrote %d bytes to socket\n", n)
 	}
+}
+
+func parseRequest(str string) (Request, error) {
+	req := Request{}
+	parts := strings.Split(str, "\r\n")
+	if len(parts) < 2 {
+		return req, fmt.Errorf("Invalid request: %s", str)
+	}
+
+	// parse command
+	if parts[0][0] != '*' {
+		return req, fmt.Errorf("Invalid request: %s", str)
+	}
+
+	countArgs, err := strconv.Atoi(parts[0][1:])
+	if err != nil {
+		return req, fmt.Errorf("Invalid request: %s", str)
+	}
+
+	if len(parts) != countArgs+1 {
+		return req, fmt.Errorf("Invalid request: %s", str)
+	}
+
+	for _, p := range parts[1:] {
+		if p[0] != '$' {
+			return req, fmt.Errorf("Invalid request: %s", str)
+		}
+
+		countBytes, err := strconv.Atoi(p[1:])
+		if err != nil {
+			return req, fmt.Errorf("Invalid request: %s", str)
+		}
+
+		if len(parts) != countBytes+1 {
+			return req, fmt.Errorf("Invalid request: %s", str)
+		}
+
+		req.args = append(req.args, parts[1])
+	}
+
+	switch req.args[0] {
+	case "PING":
+		req.cmd = CommandPing
+	case "ECHO":
+		req.cmd = CommandEcho
+	default:
+		return req, fmt.Errorf("Invalid request: %s", str)
+	}
+
+	return req, nil
 }
