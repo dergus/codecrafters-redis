1b9c6c88872c73f2f672cbf54690e027f3824817
Ziy Shem <dergesus@gmail.com>
2022-08-06 13:33:44 +0400

    key expiration

--- app/server.go
+++ app/server.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type Command string
@@ -25,8 +26,42 @@ type Request struct {
 	args [][]byte
 }
 
-var db = make(map[string][]byte)
-var mu = &sync.RWMutex{}
+type Store struct {
+	mu   *sync.RWMutex
+	data map[string]Value
+}
+
+func (s *Store) set(key string, value []byte, expires int64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[key] = Value{data: value, expires: expires}
+}
+
+func (s *Store) get(key string) []byte {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.data[key].data
+}
+
+func (s *Store) gc() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for k, v := range s.data {
+		if v.expires > 0 && v.expires < time.Now().UnixMilli() {
+			delete(s.data, k)
+		}
+	}
+}
+
+type Value struct {
+	data    []byte
+	expires int64
+}
+
+var store = Store{
+	mu:   &sync.RWMutex{},
+	data: map[string]Value{},
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -34,6 +69,8 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
+	go store.gc()
 
 	for {
 		conn, err := l.Accept()
@@ -64,10 +101,28 @@ func handleConnection(conn net.Conn) {
 			msg := append([]byte("+"), req.args[1]...)
 			resp = []byte(string(msg) + "\r\n")
 		case CommandSet:
-			set(string(req.args[1]), req.args[2])
+			var expires int64
+			for i, arg := range req.args[2:] {
+				switch strings.ToUpper(string(arg)) {
+				case "XP":
+					expires, err = strconv.ParseInt(string(req.args[i+1]), 10, 64)
+					if err != nil {
+						resp = []byte("-ERR invalid expires\r\n")
+					}
+					break
+				default:
+					resp = []byte("-ERR invalid argument\r\n")
+				}
+			}
+
+			if len(resp) != 0 {
+				break
+			}
+
+			store.set(string(req.args[1]), req.args[2], expires)
 			resp = []byte("+OK\r\n")
 		case CommandGet:
-			val := get(string(req.args[1]))
+			val := store.get(string(req.args[1]))
 			if val == nil {
 				resp = []byte("$-1\r\n")
 			} else {
@@ -150,16 +205,4 @@ func parseRequest(r io.Reader) (Request, error) {
 	}
 
 	return req, nil
-}
-
-func set(key string, value []byte) {
-	mu.Lock()
-	defer mu.Unlock()
-	db[key] = value
-}
-
-func get(key string) []byte {
-	mu.RLock()
-	defer mu.RUnlock()
-	return db[key]
 }
